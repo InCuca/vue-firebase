a0af84c86219e96b8ecc67e183ddcf5b04aff10b
Walker Leite <walker@incuca.com.br>
2018-02-23 16:44:11 -0300

    fix(lint): fix lint errors

--- template/config/prod.firebase.js
+++ template/config/prod.firebase.js
@@ -1,16 +1,16 @@
 module.exports = {
-  "apiKey": "{{api-key}}",
-  "authDomain": "{{project-id}}.firebaseapp.com",
-  "databaseURL": "https:{{project-id}}.firebaseio.com",
-  "projectId": "{{project-id}}",
-  "storageBucket": "{{project-id}}.appspot.com",
-  "messagingSenderId": "{{sender-id}}",
-  "hosting": {
-    "public": "public",
-    "rewrites": [
+  'apiKey': '{{api-key}}',
+  'authDomain': '{{project-id}}.firebaseapp.com',
+  'databaseURL': 'https:{{project-id}}.firebaseio.com',
+  'projectId': '{{project-id}}',
+  'storageBucket': '{{project-id}}.appspot.com',
+  'messagingSenderId': '{{sender-id}}',
+  'hosting': {
+    'public': 'public',
+    'rewrites': [
       {
-        "source": "**",
-        "destination": "/index.html"
+        'source': '**',
+        'destination': '/index.html'
       }
     ]
   }
